41ed96ddcfef22b1d889a59088b2c331ca599e86
Lucas Banegas <131389097+lmbanegas@users.noreply.github.com>
2025-07-14 17:16:16 -0300

    Update OpenAiHelpers.js

--- utils/OpenAiHelpers.js
+++ utils/OpenAiHelpers.js
@@ -8,7 +8,8 @@ const openai = new OpenAI({
   });
 
 
-  export async function chatHelper(message, model = "gpt-4o-mini", systemConfiguration = "You are a helpful assistant.", messageHistory = []) {
+
+export async function chatHelper(message, model = "gpt-4o-mini", systemConfiguration = "You are a helpful assistant.", messageHistory = []) {
     const completion = await openai.chat.completions.create({
       model: model,
       messages: [
@@ -16,8 +17,14 @@ const openai = new OpenAI({
         ...messageHistory,
         message,
       ],
+
+    
     });
-  
+    messageHistory.push({role: "user", messages});
+    messageHistory.push({role: "assistant", content: completion.choices[0].message.content});
     // Return a simplified object.
+    console.log(messageHistory);
     return { role: "assistant", content: completion.choices[0].message.content };
   }
+
+
